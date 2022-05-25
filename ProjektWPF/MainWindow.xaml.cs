d338ae286f525d2eb34a0f51ebdba1d06694a0fd
piotrwincencik <wincencikpiotr@gmail.com>
2022-05-25 10:38:23 +0200

    new

--- ProjektWPF/MainWindow.xaml.cs
+++ ProjektWPF/MainWindow.xaml.cs
@@ -1,9 +1,13 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.Globalization;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -23,8 +27,62 @@ namespace ProjektWPF
     /// </summary>
     public partial class MainWindow : Window
     {
+        class TableRates
+        {
+            public string Table { get; set; }
+
+            [JsonPropertyName("no")]
+            public string Number { get; set; }
+
+            [JsonPropertyName("tradingDate")]
+            public string TradingDate { get; set; }
+
+            [JsonPropertyName("effectiveDate")]
+            public string EffectiveDate { get; set; }
+
+            [JsonPropertyName("rates")]
+            public List<JsonRate> Rates { get; set; }
+        }
+
+
+
         record Rate(string Currency, string Code, decimal Ask, decimal Bid);
+
+        record JsonRate
+        {
+            [JsonPropertyName("currency")]
+            public string Currency { get; set; }
+
+            [JsonPropertyName("code")]
+            public string Code { get; set; }
+
+            [JsonPropertyName("ask")]
+            public decimal Ask { get; set; }
+
+            [JsonPropertyName("bid")]
+            public decimal Bid { get; set; }
+
+        }
+
         Dictionary<string, Rate> Rates = new Dictionary<string, Rate>();
+
+        private void DonwloadDataJson()
+        {
+            WebClient client = new WebClient();
+            client.Headers.Add("Accept", "application/json");
+            string json = client.DownloadString("http://api.nbp.pl/api/exchangerates/tables/C");
+            List<TableRates> tableRates = JsonSerializer.Deserialize<List<TableRates>>(json);
+            if (tableRates.Count == 1)
+            {
+                tableRates[0].Rates.Add(new JsonRate() { Currency = "złoty", Code = "PLN", Ask = 1, Bid = 1 });
+                foreach (JsonRate rate in tableRates[0].Rates )
+                {
+                    Rates.Add(rate.Code, new Rate(rate.Currency, rate.Code, rate.Ask, rate.Bid));
+                }
+            }
+        }
+
+
 
         private void DonwloadData()
         {
@@ -50,6 +108,7 @@ namespace ProjektWPF
             {
                 Rates.Add(rate.Code, rate);
             }
+            Rates.Add("PLN", new Rate("złoty", "PLN", 1, 1));
         }
 
 
@@ -60,7 +119,15 @@ namespace ProjektWPF
         {
             InitializeComponent();
             DonwloadData();
-            foreach(string code in Rates.Keys)
+            UpdateGui();
+
+        }
+
+        private void UpdateGui()
+        {
+            InputCurrencyCode.Items.Clear();
+            OutputCurrencyCode.Items.Clear();
+            foreach (string code in Rates.Keys)
             {
                 InputCurrencyCode.Items.Add(code);
                 OutputCurrencyCode.Items.Add(code);
@@ -68,8 +135,46 @@ namespace ProjektWPF
 
             InputCurrencyCode.SelectedIndex = 0;
             OutputCurrencyCode.SelectedIndex = 1;
+        }
 
+        private void LoadFileButton_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Title = "Wczytaj plik z notowaniami";
+            dialog.Filter = "Plikt tekstowy (*.txt)|*.txt";
+            dialog.DefaultExt = "*.txt";
+            if (dialog.ShowDialog() == true)
+            {
+                if(File.Exists(dialog.FileName))
+                {
+                    string[] lines = File.ReadAllLines(dialog.FileName);
+                    Rates.Clear();
+                    foreach(string line  in lines)
+                    {
+                        string[] tokens = line.Split(";");
+                        Rate rate = new Rate(
+                        
+                            tokens[0],
+                            tokens[1],
+                            decimal.Parse(tokens[2]),
+                            decimal.Parse(tokens[3])
+                        );
+                        Rates.Add(rate.Code, rate);
+                    }
+                    UpdateGui();
+                }
+            }
+        }
 
+        private void SaveFileButton_Click(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog dialog = new SaveFileDialog();
+            dialog.Filter = "Dokument w formacie json (*.json) |*.json";
+            dialog.Title = "Zapisz notowania do pliku JSON";
+            if (dialog.ShowDialog() == true)
+            {
+                File.WriteAllText(dialog.FileName, JsonSerializer.Serialize(Rates));
+            }
         }
 
         private void CalcOutput(object sender, RoutedEventArgs e)
@@ -78,23 +183,23 @@ namespace ProjektWPF
             string outputCode = (string)OutputCurrencyCode.SelectedItem;
 
             string amountStr = InputValue.Text;
-            decimal amount = decimal.Parse(amountStr);
+            if (decimal.TryParse(amountStr, out decimal amount))
+            {
+                Rate inputRate = Rates[inputCode];
+                Rate outputRate = Rates[outputCode];
+                decimal output = amount * inputRate.Ask / outputRate.Ask;
 
-            Rate inputRate = Rates[inputCode];
-            Rate outputRate = Rates[outputCode];
-            decimal output = amount * inputRate.Ask / outputRate.Ask;
-
-            OutputValue.Text = output.ToString();
+                OutputValue.Text = output.ToString();
+            }
         }
 
         private void NumberValidation(object sender, TextCompositionEventArgs e)
         {
-            string input = e.Text;
-            if (input.EndsWith(","))
+            e.Handled = !decimal.TryParse(InputValue.Text + e.Text, out decimal values);
+            if ((InputValue.Text + e.Text).Equals(""))
             {
-                input += "0";
+                InputValue.Text = "0";
             }
-            e.Handled = !decimal.TryParse(input, out decimal values);
         }
     }
 }
